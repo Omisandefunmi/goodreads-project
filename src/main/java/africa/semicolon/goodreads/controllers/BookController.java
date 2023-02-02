e6788c756cb8824443a7018907265112efc7573b
omisandefunmi <omisandefunmi@gmail.com>
2023-02-02 02:11:47 +0100

    "added mail template"

--- src/main/java/africa/semicolon/goodreads/controllers/BookController.java
+++ src/main/java/africa/semicolon/goodreads/controllers/BookController.java
@@ -1,4 +1,23 @@
 package africa.semicolon.goodreads.controllers;
 
+import africa.semicolon.goodreads.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class BookController {
+    private BookService bookService;
+    private List<String> validFileExtension;
+    private List<String> validImageExtension;
+
+    public BookController(BookService bookService, List<String> validFileExtension, List<String> validImageExtension) {
+        this.bookService = bookService;
+        this.validFileExtension = new ArrayList<>();
+        this.validImageExtension = new ArrayList<>();
+    }
+
+
 }
