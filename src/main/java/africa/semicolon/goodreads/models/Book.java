e551147e511c936ee5e082ba81ee56aba8c8e13b
omisandefunmi <omisandefunmi@gmail.com>
2023-02-02 01:50:27 +0100

    "refactored configurations"

--- src/main/java/africa/semicolon/goodreads/models/Book.java
+++ src/main/java/africa/semicolon/goodreads/models/Book.java
@@ -2,12 +2,20 @@ package africa.semicolon.goodreads.models;
 
 import africa.semicolon.goodreads.models.enums.AgeRate;
 import africa.semicolon.goodreads.models.enums.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Document("books")
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
     @Id
     private String id;
@@ -16,6 +24,9 @@ public class Book {
     private String author;
     private String description;
     private String coverImageUrl;
+
+    private LocalDateTime dateUploaded;
+    private String uploadedBy;
     private Category category;
 
 
---
+++ src/main/java/africa/semicolon/goodreads/services/BookService.java
@@ -0,0 +1,9 @@
+package africa.semicolon.goodreads.services;
+
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public interface BookService {
+    CompletableFuture<Map<String, String>> generateUploadURLs(String fileExtension, String imageExtension) throws ExecutionException, InterruptedException;
+}
